7330293e11e20ef1ab1255577f03b785a9309c6e
Lam Wei Li <lam_wei_li@hotmail.com>
2023-03-14 20:14:51 +0800

    refactor(test): test suite grouping

--- test/index.js
+++ test/index.js
@@ -32,21 +32,23 @@ describe("express", () => {
     app.server.start();
   });
 
-  describe("favicon", () => {
-    it("should return 204", (done) => {
-      request(app.server).get("/favicon.ico").expect(204, done);
+  describe("routing", () => {
+    describe("favicon", () => {
+      it("should return 204", (done) => {
+        request(app.server).get("/favicon.ico").expect(204, done);
+      });
     });
-  });
 
-  describe("path found", () => {
-    it("should return 200", (done) => {
-      request(app.server).get("/").expect(200, done);
+    describe("path found", () => {
+      it("should return 200", (done) => {
+        request(app.server).get("/").expect(200, done);
+      });
     });
-  });
 
-  describe("path not found", () => {
-    it("should return 404", (done) => {
-      request(app.server).get("/random").expect(404, done);
+    describe("path not found", () => {
+      it("should return 404", (done) => {
+        request(app.server).get("/random").expect(404, done);
+      });
     });
   });
 
